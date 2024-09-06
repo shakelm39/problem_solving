b5009f89d2d5e2123991e3f53a9e5eeb5e15c76d
Md Shakel Kawsar <shakelm39@gmail.com>
2024-09-06 15:02:24 +0600

    update factorial

--- index.php
+++ index.php
@@ -68,17 +68,18 @@
       <?php 
         $sum = 0;
         for($i=1; $i <=10; $i++ ){
-            $sum = $sum+$i;
+            $sum += $i;
         }
         echo "The sum of numbers from 1 to 10 is: ".$sum;
       ?>
      <!-- problem #5 solution printing the sum of  1 to 10  -->
      <!-- problem #6 solution printing 10!  -->
-      <h2>factorial 10</h2>
+      <h2>Factorial 10</h2>
       <?php 
         $factorial = 1;
         for($i=1; $i <=10; $i++ ){
-            $factorial = $factorial * $i;
+            $factorial *= $i;
+            
         }
         echo "The factorial 10 is: ".$factorial;
       ?>
